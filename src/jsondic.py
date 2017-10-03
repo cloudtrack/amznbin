a035f95e86df05503b8b12a90c26b9227f2dfb31
SeoJungHong <hsj0223@gmail.com>
2017-10-03 20:27:00 +0900

    process_metadata 에서 target_vector 도 처리하기

--- src/jsondic.py
+++ src/jsondic.py
@@ -3,41 +3,6 @@ import json
 from constants import ASIN_INDEX_FILE, INDEX_ASIN_FILE, RAW_METADATA_FILE, METADATA_FILE
 
 
-# make dic and save to json
-def make_dic():
-    print("load raw_metadata.json file")
-    with open(RAW_METADATA_FILE) as raw_metadata_file:
-        raw_metadata = json.load(raw_metadata_file)
-    total_count = len(raw_metadata)-1
-    asin_index_map = {}
-    index_asin_map = {}
-    index = 0
-    count = 1
-    for data in raw_metadata:
-        if not data.get('DATA'):
-            continue
-        if count % 1000 == 0:
-            print("make asin:index map, processing (%d/%d)..." % (count, total_count))
-        for asin in data['DATA'].keys():
-            if asin not in asin_index_map.keys():
-                asin_index_map[asin] = index
-                index_asin_map[index] = asin
-                index += 1
-        count += 1
-
-    print("dumping asin_index_file")
-    with open(ASIN_INDEX_FILE, 'w') as asin_index_file:
-        json.dump(asin_index_map, asin_index_file)
-
-    print("dumping index_asin_file")
-    with open(INDEX_ASIN_FILE, 'w') as index_asin_file:
-        json.dump(index_asin_map, index_asin_file)
-    print("Done processing target vector data")
-
-if __name__ == '__main__':
-    make_dic()
-
-
 def json2tv(index_list):
     with open(RAW_METADATA_FILE) as raw_metadata_file:
         raw_metadata = json.load(raw_metadata_file)
--- src/process_metadata.py
+++ src/process_metadata.py
@@ -1,9 +1,9 @@
 import json
 
-from constants import METADATA_DIR, RAW_METADATA_FILE, METADATA_FILE, TOTAL_DATA_SIZE
+from constants import METADATA_DIR, RAW_METADATA_FILE, METADATA_FILE, TOTAL_DATA_SIZE, ASIN_INDEX_FILE, INDEX_ASIN_FILE
 
 
-def get_raw_metadata():
+def make_raw_metadata():
     """
     metadata 각 파일들에서 필요한 정보만 모은 리스트를 리턴한다.
     리스트의 index 는 파일 이름과 동일함 (즉, 0에는 dummy data)
@@ -26,7 +26,7 @@ def get_raw_metadata():
     raw_metadata = [{}]
     for i in range(1, TOTAL_DATA_SIZE+1):
         if i % 1000 == 0:
-            print("get_raw_metadata: processing (%d/%d)..." % (i, TOTAL_DATA_SIZE))
+            print("make_raw_metadata - processing (%d/%d)..." % (i, TOTAL_DATA_SIZE))
         file_path = '%s%05d.json' % (METADATA_DIR, i)
         json_data = json.loads(open(file_path).read())
         processed_json_data = {
@@ -45,11 +45,11 @@ def get_raw_metadata():
     return raw_metadata
 
 
-def get_metadata(raw_metadata):
+def make_metadata(raw_metadata):
     metadata = {}
     for i in range(1, len(raw_metadata)):
         if i % 1000 == 0:
-            print("get_metadata: processing (%d/%d)..." % (i, TOTAL_DATA_SIZE))
+            print("make_metadata - processing (%d/%d)..." % (i, TOTAL_DATA_SIZE))
         if raw_metadata[i]:
             quantity = raw_metadata[i]['TOTAL']
             if quantity > 0:
@@ -72,14 +72,40 @@ def get_metadata(raw_metadata):
     return metadata
 
 
+def make_target_vector_map(raw_metadata):
+    asin_index_map = {}
+    index_asin_map = {}
+    index = 0
+    for i in range(1, len(raw_metadata)):
+        if i % 1000 == 0:
+            print("make_target_vector_map - processing (%d/%d)..." % (i, TOTAL_DATA_SIZE))
+        data = raw_metadata[i]
+        for asin in data['DATA'].keys():
+            if asin not in asin_index_map.keys():
+                asin_index_map[asin] = index
+                index_asin_map[index] = asin
+                index += 1
+
+    return asin_index_map, index_asin_map
+
+
 if __name__ == '__main__':
-    raw_metadata = get_raw_metadata()
-    print("dumping raw_metadata_file")
+    raw_metadata = make_raw_metadata()
+    print("dumping " + RAW_METADATA_FILE)
     with open(RAW_METADATA_FILE, 'w') as raw_metadata_file:
         json.dump(raw_metadata, raw_metadata_file)
 
-    metadata = get_metadata(raw_metadata)
-    print("dumping metadata_file")
+    metadata = make_metadata(raw_metadata)
+    print("dumping " + METADATA_FILE)
     with open(METADATA_FILE, 'w') as metadata_file:
         json.dump(metadata, metadata_file)
-    print("Done processing metadata")
+
+    asin_index_map, index_asin_map = make_target_vector_map(raw_metadata)
+    print("dumping " + ASIN_INDEX_FILE)
+    with open(ASIN_INDEX_FILE, 'w') as asin_index_file:
+        json.dump(asin_index_map, asin_index_file)
+    print("dumping " + INDEX_ASIN_FILE)
+    with open(INDEX_ASIN_FILE, 'w') as index_asin_file:
+        json.dump(index_asin_map, index_asin_file)
+
+    print("Done processing metadata!")
