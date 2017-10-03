70edc203db7569c7bfc0b2eee2fadf142d69520c
SeoJungHong <hsj0223@gmail.com>
2017-10-03 17:33:51 +0900

    constant 정의

---
+++ src/constants.py
@@ -0,0 +1,13 @@
+# Directory path relative to project root
+DATASET_DIR = "dataset/"
+IMAGE_DIR = "dataset/bin-images/"
+METADATA_DIR = "dataset/metadata/"
+
+# File path relative to project root
+METADATA_FILE = "dataset/metadata.json"
+RAW_METADATA_FILE = "dataset/raw_metadata.json"
+ASIN_INDEX_FILE = "dataset/asin_index_map.json"
+INDEX_ASIN_FILE = "dataset/index_asin_map.json"
+
+TOTAL_DATA_SIZE = 535234
+IMAGE_SIZE = 224
