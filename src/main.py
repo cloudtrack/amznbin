e8ab1ab26a831af43bd95dabe0b355a3dfc745b5
Ji Eun Lee <dreamhunter@snu.ac.kr>
2017-11-04 01:37:47 +0900

    reduce validation size

--- src/main.py
+++ src/main.py
@@ -37,7 +37,7 @@ def train(model, sess, saver, train_data, valid_data, batch_size, max_iters, use
             train_error = model.eval_loss(batch_image, batch_target)
             train_rmse, train_acc, train_pred = model.eval_metric(batch_image, batch_target)
 
-            valid_batch_image, valid_batch_target = valid_data.next_batch(batch_size)
+            valid_batch_image, valid_batch_target = valid_data.next_batch(100)
             valid_rmse, valid_acc, valid_pred = model.eval_metric(valid_batch_image, valid_batch_target)
             print(model.model_filename)
             print("train accuracy: %.4f, valid accuracy: %.4f, train rmse: %.4f, valid rmse: %.4f, train loss: %.4f in %ds"
